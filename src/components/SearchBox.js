9140178b35606b2f26c9de88196f85344aa9edb0
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-01 23:45:04 +0900

    lint fix

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const SearchBox = () => {
   let [keyword, setKeyword] = useState("");
